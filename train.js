4002a24f536d4b357ccc5a35eff4f3fb8c600c06
ShakhriyorNorkobilovich <shaxroyorbek2101@gmail.com>
2023-09-18 21:28:05 -0700

    fix

--- train.js
+++ train.js
@@ -1,7 +1,7 @@
 function checkSimilarity(shoptoli1, shoptoli2) {
   
-  //shoptoli1 = shoptoli1;
-  //shoptoli2 = shoptoli2;
+  shoptoli1 = shoptoli1;
+  shoptoli2 = shoptoli2;
   
   return shoptoli1.split('').sort().join('') === shoptoli2.split('').sort().join('');
 }
